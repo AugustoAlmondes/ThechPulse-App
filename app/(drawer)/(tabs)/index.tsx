958cc15641bbdf4c794c219d0332613541c8af31
AugustoAlmondes <augusto7666@gmail.com>
2026-05-04 13:46:56 -0300

    style: the button layout has been improved

--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -243,10 +243,10 @@ const styles = StyleSheet.create({
     button: {
         alignSelf: 'flex-start',
         flexDirection: 'row',
-        paddingHorizontal: 15,
-        paddingVertical: 4,
-        marginTop: 20,
-        borderRadius: 2,
+        paddingHorizontal: 24,
+        paddingVertical: 10,
+        marginTop: 10,
+        borderRadius: 10,
         gap: 10,
         justifyContent: 'center',
         alignItems: 'center'
